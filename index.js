114776951a2eb29a23302a0f901be90873e37a95
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2024-11-07 12:03:27 +0300

    latest version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let inputItem = document.getElementById("newItems");
 let write = document.getElementById("list1");
 let list;
 let listWrite;
+const storagekey ="newItems"
 
 function condition() {
   if (inputItem.value === "") {
@@ -22,4 +23,16 @@ function removeItems() {
   let p = document.querySelector("ul");
   let q = document.querySelector("li:last-child");
   p.remove(q);
+}
+
+function saveItems(){
+  const stringItems = JSON.stringify(items);
+  localStorage.setItem(storagekey,stringItems)
+
+}
+function loadItems(){
+  const oldItems = localStorage.getItem(storagekey)
+  if(oldItems) items = JSON.parse(oldItems)
+  condition()
+
 }
